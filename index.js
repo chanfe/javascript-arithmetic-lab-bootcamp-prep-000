d031fbb045130c5c82eecabff623a17c2754b980
Felix Chan <felixchan29@gmail.com>
2018-06-19 01:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function add(a,b){
   return a+b;
 }
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiplies(a,b){
+  return a*b;
+}
+
+function divides(a,b){
+  return a/b;
+}
+
+function inc(a){
+  return a++;
+}
